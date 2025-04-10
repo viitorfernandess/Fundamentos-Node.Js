00649266016d5120af59b00f7b6df686804fe04d
viitorfernandess <vitorazevedofer007@gmail.com>
2025-04-10 14:19:06 -0300

    unlink.js
    Excluir um arquivo

---
+++ 01-Manipulando-arquivos/unlink.js
@@ -0,0 +1,10 @@
+const fs = require("node:fs")
+
+fs.unlink("arquivo.csv", (error) => {
+    if(error) {
+        console.log("Erro ao excluir o arquivo.", error.message)
+        return
+    }
+
+    console.log("Arquivo excluído com sucesso!")
+})
