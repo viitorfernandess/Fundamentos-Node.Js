7fd43b807ad0ab75f81b781e36ce75a79c47ba9f
viitorfernandess <vitorazevedofer007@gmail.com>
2025-04-11 17:15:45 -0300

    read-file.js

--- 02-Exercicio-01/read-file.js
+++ 02-Exercicio-01/read-file.js
@@ -10,3 +10,5 @@ fs.readFile("meuarquivo.txt", "utf-8", (error, data) => {
     console.log("Conteúdo do arquivo: ", data)
 
 })
+
+// Na callback de readFile, você recebe dois argumentos: error e data. O segundo (data) é o conteúdo lido do arquivo, e você precisa usá-lo.
