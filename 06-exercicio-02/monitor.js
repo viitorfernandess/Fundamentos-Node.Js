4b4dd951413d7f0decf37d8c4d8278de1bc0b1ae
viitorfernandess <vitorazevedofer007@gmail.com>
2025-04-16 10:24:36 -0300

    monitor.js
    Criada a função para identificar o sistema utilizado pelo usuário

---
+++ 06-exercicio-02/monitor.js
@@ -0,0 +1,22 @@
+// Começar importando os módulos que serão utilizados
+
+const os = require("node:os")
+const fs = require("node:fs")
+const path = require("node:path")
+
+//Criar um mapa para o sistema saber o nome correto de cada plataforma
+
+const systemPlatformMap = {
+    "win32": "Windows",
+    "linux": "Linux",
+    "darwin": "MacOS"
+}
+
+function printlog() {
+    const system = systemPlatformMap[os.platform()]
+
+    console.log("DETALHES DO SISTEMA: ")
+    console.log(`Sistema Operacional: ${system}`)
+}
+
+printlog()
